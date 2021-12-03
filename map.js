9a1fe7dc4ec95e5baba43f591f96dbeb93c9f08f
mjjstockman <56722629+mjjstockman@users.noreply.github.com>
2021-12-03 12:29:57 +0000

    Add additional example

--- map.js
+++ map.js
@@ -49,3 +49,42 @@ const students = [
 // return an array containing the name and id values of all in students array
 const studentsWithIds = students.map(student => [student.name, student.id]);
 console.log(studentsWithIds);
+
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+
+let [john, ...rest] = students.map(itm => [itm.name, itm.results]);
+console.log(john);
+console.log(rest);
+
+
+
+
+
+
